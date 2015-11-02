8aa6aee296dfb3db7effc7ae9d0e193406b0e1f2
Harlan Connor <harlan.connor@gmail.com>
2015-11-02 15:58:46 +0000

    First

---
+++ Node/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open Cyclic3.Alpha
+let help() = ()
+[<EntryPoint>]
+let main argv = 
+    let args = argv |> Seq.pairwise |> Map.ofSeq
+    if Array.contains "-h" argv || Array.contains "--help" argv || Array.contains "/?" argv then help();exit 0
+    args.[0]
+    0 // return an integer exit code
